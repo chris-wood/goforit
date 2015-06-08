793ac4088b95b4430881117ee7562e4d7c3dc0e0
Christopher Wood <christopherwood07@gmail.com>
2015-06-08 07:25:39 -0700

    Select updated

--- select.go
+++ select.go
@@ -1,40 +1,45 @@
-// Go's _select_ lets you wait on multiple channel
-// operations. Combining goroutines and channels with
-// select is a powerful feature of Go.
-
 package main
 
 import "time"
 import "fmt"
+import "strconv"
+import "math/rand"
 
 func main() {
 
-    // For our example we'll select across two channels.
-    c1 := make(chan string)
-    c2 := make(chan string)
+    c1 := make(chan bool)
+    c2 := make(chan int)
+    c3 := make(chan string)
 
-    // Each channel will receive a value after some amount
-    // of time, to simulate e.g. blocking RPC operations
-    // executing in concurrent goroutines.
+    var input string // uninitialized
+    fmt.Scanln(&input)
+    waitStep, _ := strconv.Atoi(input)
+    wait := int32(waitStep)
+
+    rand.Seed(42)
+
+    // start each in a separate goroutine
     go func() {
-        time.Sleep(time.Second * 1)
-        c1 <- "one"
+        time.Sleep(time.Second * time.Duration(rand.Int31n(wait)))
+        c1 <- true
+    }()
+        go func() {
+        time.Sleep(time.Second * time.Duration(rand.Int31n(wait)))
+        c2 <- 2
     }()
     go func() {
-        time.Sleep(time.Second * 2)
-        c2 <- "two"
+        time.Sleep(time.Second * time.Duration(rand.Int31n(wait)))
+        c3 <- "three"
     }()
 
-    // We'll use `select` to await both of these values
-    // simultaneously, printing each one as it arrives.
-	// NOTE: select basically chooses a block based on channel-generated events
-    for i := 0; i < 2; i++ {
-        select {
-        case msg1 := <-c1:
-            fmt.Println("received", msg1)
-        case msg2 := <-c2:
-            fmt.Println("received", msg2)
-        }
+    // see which one gets here first
+    select {
+    case msg1 := <-c1:
+        fmt.Printf("received: %t\n", msg1)
+    case msg2 := <-c2:
+        fmt.Printf("received: %d\n", msg2)
+    case msg3 := <-c3:
+        fmt.Printf("received: %s\n", msg3)
     }
 }
 
